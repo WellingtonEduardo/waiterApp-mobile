3fd8ddf430c7bf0e97a4e4d95024e5107c9aac34
Wellington <wellingtoneduardo16@outlook.com>
2024-01-25 10:55:44 -0300

    I finished the category component, the designer part still needs to implement the logic to make requests

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -2,8 +2,17 @@ import { FlatList } from 'react-native';
 import { categories } from '../../mocks/categories';
 import { Text } from '../Text';
 import { Category, Icon } from './styles';
+import { useState } from 'react';
 
 export function Categories() {
+  const [selectedCategory, setSelectedCategory] = useState<null | string>(null);
+
+
+  function handleSelectCategory(categoryId: string) {
+    const category = selectedCategory === categoryId ? null : categoryId;
+    setSelectedCategory(category);
+  }
+
   return (
     <FlatList
       horizontal
@@ -11,16 +20,22 @@ export function Categories() {
       data={categories}
       contentContainerStyle={{ paddingRight: 24 }}
       keyExtractor={category => category._id}
-      renderItem={({ item: category }) => (
-        <Category>
-          <Icon>
-            <Text>{category.icon}</Text>
-          </Icon>
-          <Text size={14} weight='Semibold'>
-            {category.name}
-          </Text>
-        </Category>
-      )}
+      renderItem={({ item: category }) => {
+        const isSelected = selectedCategory === category._id;
+
+        return (
+          <Category onPress={() => handleSelectCategory(category._id)}>
+            <Icon>
+              <Text opacity={isSelected ? 1 : 0.5}>
+                {category.icon}
+              </Text>
+            </Icon>
+            <Text size={14} weight='Semibold' opacity={isSelected ? 1 : 0.5}>
+              {category.name}
+            </Text>
+          </Category>
+        );
+      }}
     />
 
   );
--- src/components/Categories/styles.ts
+++ src/components/Categories/styles.ts
@@ -1,15 +1,16 @@
+import { Platform } from 'react-native';
 import styled from 'styled-components/native';
 
+const isAndroid = Platform.OS === 'android';
 
-
-export const Category = styled.View`
+export const Category = styled.TouchableOpacity`
   align-items: center;
   margin-left: 24px;
 `;
 
 
 
-export const Icon =  styled.View`
+export const Icon = styled.View`
   background-color: #fff;
   width: 44px;
   height: 44px;
@@ -17,4 +18,6 @@ export const Icon =  styled.View`
   justify-content: center;
   align-items: center;
   margin-bottom: 8px;
+  box-shadow: 0px 2px 1px rgba(0,0,0, ${isAndroid ? 1 : 0.1});
+  elevation: 2;
  `;
